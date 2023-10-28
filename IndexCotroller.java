96ce86cf7ce33616aaeb178d38c17e706b451f95
Siddhu Sinnur <114195263+SiddhuSinnur@users.noreply.github.com>
2023-10-28 12:09:00 +0530

    Add files via upload

---
+++ IndexCotroller.java
@@ -0,0 +1,31 @@
+package com.okayjava.html.cotroller;
+
+import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import com.okayjava.html.model.User;
+			
+
+@Controller
+public class IndexCotroller {
+	
+	@GetMapping("/")
+	public String index() {
+		
+		return "index";
+	}
+	
+	
+	@PostMapping("/register")
+	public String userResistration(@ModelAttribute User user, Model modee) {
+		System.out.println(user.toString());
+		//validate
+		System.out.println(user.getFname());
+		System.out.println(user.getLname());
+		System.out.println(user.getEmail());
+
+		return "welcome";
+	}
+
+}
